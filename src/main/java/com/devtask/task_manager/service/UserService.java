56556372c45c7468bb163658d4c46c11ea1aab88
Nika Oniani <nikaoniani2018@gmail.com>
2024-09-30 15:08:05 +0400

    Implement UserService interface and UserServiceImpl class for user CRUD operations

--- src/main/java/com/devtask/task_manager/service/UserService.java
+++ src/main/java/com/devtask/task_manager/service/UserService.java
@@ -1,4 +1,12 @@
 package com.devtask.task_manager.service;
 
+import java.util.List;
+import com.devtask.task_manager.entity.UserEntity;
+
 public interface UserService {
+    List<UserEntity> getAllUsers();
+    UserEntity createUser(UserEntity user);
+    UserEntity updateUser(Long id, UserEntity user);
+    void deleteUser(Long id);
+    UserEntity getUserById(Long id);
 }
